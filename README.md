e94af69c82f779084f1223c7ce35b1cefa346d96
yugosh <38684733+yugosh@users.noreply.github.com>
2024-08-26 22:52:26 +0700

    UPDATE MAIN

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# VUE-REPORT-BUILDER
- 
+# report-builder
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+header footer 
+data keluaran 100 bisa 2 page lebih tanpa terpotong
+responsive
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Report Builder</h1>
+    <ReportCanvas />
+  </div>
+</template>
+
+<script>
+import ReportCanvas from './components/ReportCanvas.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ReportCanvas,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
